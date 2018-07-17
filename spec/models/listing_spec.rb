30bfd5be7bc9b315441856574367f0ef1cf518c3
Vishal Sharma <vsh1819@gmail.com>
2018-07-17 11:50:41 +0000

    automatically backed up by learn

--- spec/models/listing_spec.rb
+++ spec/models/listing_spec.rb
@@ -8,7 +8,7 @@ describe Listing do
   let!(:listing) do
     Listing.create(
       address: '123 Main Street',
-      # listing_type: "private room",
+      listing_type: "private room",
       title: "Beautiful Apartment on Main Street",
       description: "My apartment is great. there's a bedroom. close to subway....blah blah",
       price: 50.00,
