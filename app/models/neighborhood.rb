2267983f37d8c7ad271218f1aebf7cd515083987
Vishal Sharma <vsh1819@gmail.com>
2018-07-17 11:08:15 +0000

    automatically backed up by learn

--- app/models/neighborhood.rb
+++ app/models/neighborhood.rb
@@ -4,37 +4,37 @@ class Neighborhood < ActiveRecord::Base
 
 end
 
-require 'rails_helper'
+# require 'rails_helper'
 
-describe Neighborhood do
-  let(:nyc) { City.create(name: 'NYC') }
-  let(:brighton_beach) { Neighborhood.create(name: 'Brighton Beach', city: nyc) }
+# describe Neighborhood do
+#   let(:nyc) { City.create(name: 'NYC') }
+#   let(:brighton_beach) { Neighborhood.create(name: 'Brighton Beach', city: nyc) }
 
-  it 'has a name' do
-    expect(brighton_beach.name).to eq('Brighton Beach')
-  end
+#   it 'has a name' do
+#     expect(brighton_beach.name).to eq('Brighton Beach')
+#   end
 
-  it 'belongs to a city' do
-    expect(brighton_beach.city).to be(nyc)
-  end
+#   it 'belongs to a city' do
+#     expect(brighton_beach.city).to be(nyc)
+#   end
 
-  context "listings" do
-    let(:user) { User.create(name: "Arel") }
-    let!(:listing) do
-      Listing.create(
-        address: '44 Ridge Lane',
-        listing_type: "whole house",
-        title: "Beautiful Home on Mountain",
-        description: "Whole house for rent on mountain. Many bedrooms.",
-        price: 200.00,
-        neighborhood: brighton_beach,
-        host: user
-      )
-    end
+#   context "listings" do
+#     let(:user) { User.create(name: "Arel") }
+#     let!(:listing) do
+#       Listing.create(
+#         address: '44 Ridge Lane',
+#         listing_type: "whole house",
+#         title: "Beautiful Home on Mountain",
+#         description: "Whole house for rent on mountain. Many bedrooms.",
+#         price: 200.00,
+#         neighborhood: brighton_beach,
+#         host: user
+#       )
+#     end
 
-    it 'has many listings' do
-      expect(brighton_beach.listings).to eq([listing])
-    end
-  end
-end
+#     it 'has many listings' do
+#       expect(brighton_beach.listings).to eq([listing])
+#     end
+#   end
+# end
 
