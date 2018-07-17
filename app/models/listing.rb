544e34a99bee8abf8e3f2d275bda61afaddb79a7
Vishal Sharma <vsh1819@gmail.com>
2018-07-17 11:17:07 +0000

    automatically backed up by learn

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -4,5 +4,5 @@ class Listing < ActiveRecord::Base
   belongs_to :host, :class_name => "User"
   has_many :reservations
   has_many :reviews, through: :reservations
-  has_many :guests, through: 
+  has_many :guests, through: :reservations
 end
