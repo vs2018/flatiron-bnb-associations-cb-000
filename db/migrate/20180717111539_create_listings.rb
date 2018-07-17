b1605690bf80906a6683117c23d1f24b605986aa
Vishal Sharma <vsh1819@gmail.com>
2018-07-17 11:36:33 +0000

    automatically backed up by learn

--- db/migrate/20180717111539_create_listings.rb
+++ db/migrate/20180717111539_create_listings.rb
@@ -2,6 +2,7 @@ class CreateListings < ActiveRecord::Migration
   def change
     create_table :listings do |t|
       t.string :title
+      t.string :listing_type
       t.string :description
       t.string :address
       t.float :price
